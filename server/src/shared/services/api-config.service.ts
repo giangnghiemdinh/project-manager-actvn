4353007046f305b8eb5a51081f1e27a8af18fdb8
Nghiêm Đình Giang <giangnghiemdinh@gmail.com>
2023-06-14 17:52:10 +0700

    turn on migration

--- server/src/shared/services/api-config.service.ts
+++ server/src/shared/services/api-config.service.ts
@@ -123,7 +123,7 @@ export class ApiConfigService {
       database: this.get('MYSQL_DATABASE'),
       namingStrategy: new SnakeNamingStrategy(),
       autoLoadEntities: true,
-      migrationsRun: false,
+      migrationsRun: true,
       synchronize: true,
       logging: this.isDevelopment,
     };
